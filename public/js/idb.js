aafff2f23194264a0595a44d74e78acc165c75eb
Stephen Fike <sfike98@gmail.com>
2022-07-28 14:07:23 -0500

    added links to html

--- public/js/idb.js
+++ public/js/idb.js
@@ -11,7 +11,7 @@ request.onsuccess = function(event) {
     db = event.target.result;
 
     if(navigator.online) {
-        // uploadTransaction();
+        uploadTransaction();
     }
 };
 
@@ -37,7 +37,7 @@ function uploadTransaction() {
 
     getAll.onsuccess = function() {
         if(getAll.result.length > 0) {
-            fetch('/api/transactions', {
+            fetch('/api/transaction/bulk', {
                 method: 'POST',
                 body: JSON.stringify(getAll.result),
                 headers: {
